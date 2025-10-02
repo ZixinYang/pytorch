33e2dd1303d9bfb49c91ffbd6de58bfe283149d8
Huy Do <huydhn@gmail.com>
2025-10-01 20:59:46 -0700

    Fix lint

--- benchmarks/dynamo/check_accuracy.py
+++ benchmarks/dynamo/check_accuracy.py
@@ -18,7 +18,7 @@ flaky_models = {
     # discovered in https://github.com/pytorch/pytorch/issues/161419. Its not flaky but really hard to repro, so skipping it
     "mobilenetv3_large_100",
     # https://github.com/pytorch/pytorch/issues/163670
-    "vision_maskrcnn"
+    "vision_maskrcnn",
 }
 
 
